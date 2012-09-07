9f0ccaff34ac55aa0c553061d8c137cb4d19cef9
Bruno Bonfils <asyd@asyd.net>
2012-09-07 17:01:35 -0400

    Why my apps disappears from settings.py

--- lbe/settings.py
+++ lbe/settings.py
@@ -1,6 +1,6 @@
 # Django settings for lbe project.
 import os, django
-SITE_ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+#SITE_ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -72,7 +72,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-	os.path.join(SITE_ROOT, 'static'),
+#	os.path.join(SITE_ROOT, 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -112,7 +112,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-	os.path.join(SITE_ROOT, 'templates')
+#	os.path.join(SITE_ROOT, 'templates')
 )
 
 INSTALLED_APPS = (
@@ -123,6 +123,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    'directory',
+    'config',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
