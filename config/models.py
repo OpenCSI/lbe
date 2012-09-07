3f8c0bdf463db08ad87baf65ef7ecf8881348216
Bruno Bonfils <asyd@asyd.net>
2012-09-07 13:09:50 -0400

    added models

--- config/models.py
+++ config/models.py
@@ -1,3 +1,35 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
-# Create your models here.
+class language(models.Model):
+	name = models.CharField(max_length=30)
+
+class ACL(models.Model):
+	object = models.CharField(max_length=25)
+	type = models.CharField(max_length=10)
+	attribut = models.CharField(max_length=35)
+	condition = models.CharField(max_length=100)
+	language = models.ForeignKey(language)
+
+class LDAP(models.Model):
+	hostname = models.CharField(max_length=50)
+	user = models.CharField(max_length=30)
+	pwd = models.CharField(max_length=30)
+	port = models.IntegerField()
+	base = models.CharField(max_length=30)
+	
+class MONGO(models.Model):
+	hostname = models.CharField(max_length=50)
+	port = models.IntegerField()
+	db = models.CharField(max_length=30)
+	
+class Format(models.Model):
+	name = models.CharField(max_length=64)
+	value = models.CharField(max_length=128,null = True, default = None)
+	file = models.CharField(max_length=32,null = True, default = None)
+
+class messageError(models.Model):
+	username = models.CharField(max_length=64)
+	type = models.CharField(max_length=32)# ldap, mongo, crypto, template, ...
+	message = models.CharField(max_length=1024)
+	date = models.DateTimeField(auto_now=True)
--- lbe/settings.py
+++ lbe/settings.py
@@ -11,10 +11,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'lbenew',                      # Or path to database file if using sqlite3.
+        'USER': 'lbe',                      # Not used with sqlite3.
+        'PASSWORD': 'lbepassword',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -27,7 +27,7 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Europe/Paris'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -118,6 +118,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'directory',
+    'config',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
