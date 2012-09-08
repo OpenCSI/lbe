a00466e664830c32c22478a287db9243b3ce5dc3
asyd <asyd@asyd.net>
2012-09-08 17:15:56 +0200

    Demo of messages framework

--- directory/views.py
+++ directory/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render_to_response, redirect
 from directory.models import *
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.context_processors import csrf
+from django.contrib import messages
+from django.template import RequestContext
 
-def index(requestContext):
-	return render_to_response('index.html')
+
+def index(request):
+	messages.add_message(request, messages.ERROR, 'Welcome')
+	return render_to_response('index.html', None, context_instance=RequestContext(request))
